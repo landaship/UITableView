1d2ad62534f2369946f8614979c0b981b26cfe42
landaship <landaship@163.com>
2014-07-10 12:33:13 +0800

    Initial Commit

---
+++ UITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITableView
+//
+//  Created by Louis on 14-7-10.
+//  Copyright (c) 2014年 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
